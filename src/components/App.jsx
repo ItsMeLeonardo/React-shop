181f1c1e7b3153af43bb687e93df5c525b92aaf2
Carlos Leonardao <74982565+ItsMeLeonardo@users.noreply.github.com>
2021-10-31 14:45:06 -0500

    [ADD] Sass

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../styles/global.scss";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello World without vite or react app</h1>
+    </div>
+  );
+};
+
+export default App;
