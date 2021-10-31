50e732847fd0930bf1aede03c37062c38a82065b
Carlos Leonardao <74982565+ItsMeLeonardo@users.noreply.github.com>
2021-10-31 18:03:47 -0500

    [ADD] alias to imports

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,44 +1,48 @@
-import React from 'react';
-import '../styles/Header.scss';
+import React from "react";
+import "@styles/Header.scss";
+
+import menu from "@icons/icon_menu.svg";
+import logo from "@logos/logo_yard_sale.svg";
+import iconShoppingCart from "@icons/icon_shopping_cart.svg";
 
 const Header = () => {
-	return (
-		<nav>
-			<img src="./icons/icon_menu.svg" alt="menu" className="menu" />
-			<div className="navbar-left">
-				<img src="./logos/logo_yard_sale.svg" alt="logo" className="logo" />
-				<ul>
-					<li>
-						<a href="/">All</a>
-					</li>
-					<li>
-						<a href="/">Clothes</a>
-					</li>
-					<li>
-						<a href="/">Electronics</a>
-					</li>
-					<li>
-						<a href="/">Furnitures</a>
-					</li>
-					<li>
-						<a href="/">Toys</a>
-					</li>
-					<li>
-						<a href="/">Others</a>
-					</li>
-				</ul>
-			</div>
-			<div className="navbar-right">
-				<ul>
-					<li className="navbar-email">platzi@example.com</li>
-					<li className="navbar-shopping-cart">
-						<img src="./icons/icon_shopping_cart.svg" alt="shopping cart" />
-						<div>2</div>
-					</li>
-				</ul>
-			</div>
-		</nav>
-	);
-}
+  return (
+    <nav>
+      <img src={menu} alt="menu" className="menu" />
+      <div className="navbar-left">
+        <img src={logo} alt="logo" className="logo-nav" />
+        <ul>
+          <li>
+            <a href="/">All</a>
+          </li>
+          <li>
+            <a href="/">Clothes</a>
+          </li>
+          <li>
+            <a href="/">Electronics</a>
+          </li>
+          <li>
+            <a href="/">Furnitures</a>
+          </li>
+          <li>
+            <a href="/">Toys</a>
+          </li>
+          <li>
+            <a href="/">Others</a>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <ul>
+          <li className="navbar-email">platzi@example.com</li>
+          <li className="navbar-shopping-cart">
+            <img src={iconShoppingCart} alt="shopping cart" />
+            <div>2</div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
 export default Header;
--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import ProductItem from '../components/ProductItem';
-import '../styles/ProductList.scss';
+import React from "react";
+import ProductItem from "@components/ProductItem";
+import "@styles/ProductList.scss";
 
 const ProductList = () => {
-	return (
-		<section className="main-container">
-			<div className="ProductList">
-				<ProductItem />
-			</div>
-		</section>
-	);
-}
+  return (
+    <section className="main-container">
+      <div className="ProductList">
+        <ProductItem />
+      </div>
+    </section>
+  );
+};
 
 export default ProductList;
